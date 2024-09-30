71a4c5359f9f6482d9234450c4dc7d5eae55b379
amin khoeini <amin.khoeini@onawhim.com>
2024-09-30 18:19:24 -0400

    cleanup and adding some important dependencies to android gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,15 +34,48 @@ android {
         jvmTarget = "1.8"
     }
 }
-
 dependencies {
 
+    //region androidx
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    //endregion
+
+    //region material design
+    implementation(libs.material)
+    //endregion
+
+    //region test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //endregion
+
+    //region networking(retrofit)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    //endregion
+
+
+    //region db(room)
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    //endregion
+
+    //region di(dagger)
+    implementation(libs.hilt.android)
+    //endregion
+
+    //region async
+    implementation(libs.kotlinx.coroutines.android)
+    //endregion
+
+
+
+
+
 }
